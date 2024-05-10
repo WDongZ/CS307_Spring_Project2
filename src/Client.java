906054e10a1def7239dc497ee254152b76a6bf4e
WDongZ <505314385@qq.com>
2024-05-10 22:00:59 +0800

    Database project2 update

--- src/Client.java
+++ src/Client.java
@@ -5,16 +5,6 @@ import java.sql.Statement;
 
 public class Client {
     public static void main(String[] args) throws Exception {
-        String url = "jdbc:postgresql://localhost:5432/project";
-        Connection conn = DriverManager.getConnection(url, "checker", "123456");
-        PreparedStatement st = conn.prepareStatement("update line_station set position = (select rk from (select id, rank() over (partition by line_id order by id) rk from line_station) rk where id = ?) where id = ?;");
-        for (int i = 1; i <= 373; i++) {
-            st.setInt(1, i);
-            st.setInt(2, i);
-            st.executeUpdate();
-        }
-        st.close();
-        conn.close();
-//        Login.login();
+        Login.login();
     }
 }
